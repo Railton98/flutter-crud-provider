30f7637a58323d27d1c127d6d37b26eee59e0e44
Railton98 <railtonleal98@gmail.com>
2020-05-13 16:13:29 -0300

    fake database

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:crud/models/user_model.dart';
+
+const DUMMY_USERS = {
+  '1': const UserModel(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_960_720.png',
+  ),
+  '2': const UserModel(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  ),
+  '3': const UserModel(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635464_960_720.png',
+  ),
+  '4': const UserModel(
+    id: '4',
+    name: 'Pedro',
+    email: 'pedro@email.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635454_960_720.png',
+  ),
+};
